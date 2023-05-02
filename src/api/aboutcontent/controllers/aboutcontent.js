14ff9200778db8985d86323538a5c2411b8f316d
Wojtek Opulski <wojtekopulski@gmail.com>
2023-05-02 14:04:07 +0200

    feat: add data schemas, configure postgres with hosting on render

---
+++ src/api/aboutcontent/controllers/aboutcontent.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * aboutcontent controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::aboutcontent.aboutcontent');
