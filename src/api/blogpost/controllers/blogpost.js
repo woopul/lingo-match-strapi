1857b1d42d6f4ad366c29a3d446c68f3edc2adc9
Wojtek Opulski <wojtekopulski@gmail.com>
2023-05-06 17:19:40 +0200

    feat: modify controller to fetch blogopst by slug

--- src/api/blogpost/controllers/blogpost.js
+++ src/api/blogpost/controllers/blogpost.js
@@ -1,9 +1,23 @@
-'use strict';
+"use strict";
 
 /**
  * blogpost controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::blogpost.blogpost');
+module.exports = createCoreController(
+  "api::blogpost.blogpost",
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      const { id: slug } = ctx.params;
+      const { query } = ctx;
+      if (!query.filters) query.filters = {};
+      query.filters.slug = { $eq: slug };
+      const entity = await strapi.service("api::blogpost.blogpost").find(query);
+      const { results } = await this.sanitizeOutput(entity, ctx);
+
+      return this.transformResponse(results[0]);
+    },
+  })
+);
