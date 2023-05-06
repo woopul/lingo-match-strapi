dbdce4f9083fb1848792d023ace6d060e6cd7a17
Wojtek Opulski <wojtekopulski@gmail.com>
2023-05-06 17:20:53 +0200

    feat: modify platform controller to fetch platform by slug

--- src/api/platform/controllers/platform.js
+++ src/api/platform/controllers/platform.js
@@ -1,9 +1,23 @@
-'use strict';
+"use strict";
 
 /**
  * platform controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::platform.platform');
+module.exports = createCoreController(
+  "api::platform.platform",
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      const { id: slug } = ctx.params;
+      const { query } = ctx;
+      if (!query.filters) query.filters = {};
+      query.filters.slug = { $eq: slug };
+      const entity = await strapi.service("api::platform.platform").find(query);
+      const { results } = await this.sanitizeOutput(entity, ctx);
+
+      return this.transformResponse(results[0]);
+    },
+  })
+);
