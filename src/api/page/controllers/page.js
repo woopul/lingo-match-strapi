c1956cf4a02ee0076606e17c828a43e4e6fbd099
Wojtek Opulski <wojtekopulski@gmail.com>
2024-01-07 22:38:49 +0100

    feat: add fetch page by slug

--- src/api/page/controllers/page.js
+++ src/api/page/controllers/page.js
@@ -1,9 +1,19 @@
-'use strict';
+"use strict";
 
 /**
  * page controller
  */
+const { createCoreController } = require("@strapi/strapi").factories;
 
-const { createCoreController } = require('@strapi/strapi').factories;
+module.exports = createCoreController("api::page.page", ({ strapi }) => ({
+  async findOne(ctx) {
+    const { id: slug } = ctx.params;
+    const { query } = ctx;
+    if (!query.filters) query.filters = {};
+    query.filters.slug = { $eq: slug };
+    const entity = await strapi.service("api::page.page").find(query);
+    const { results } = await this.sanitizeOutput(entity, ctx);
 
-module.exports = createCoreController('api::page.page');
+    return this.transformResponse(results[0]);
+  },
+}));
